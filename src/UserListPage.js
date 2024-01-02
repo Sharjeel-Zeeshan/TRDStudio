88a47e8bf57b10962499253eabd1652bfb418e0b
Sharjeel-Zeeshan <124786093+Sharjeel-Zeeshan@users.noreply.github.com>
2024-01-02 17:44:31 +0530

    TRDStudio Task

--- src/UserListPage.js
+++ src/UserListPage.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import "./UserListPage"
+import "./UserListPage.css"
 
 const UserListPage = () => {
   const [users, setUsers] = useState([]);
